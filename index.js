ac1e7921b1ac59cc08998539caaeabbbfdcb632f
Amir Akbari <amirmahdiakbari@khu.ac.ir>
2025-08-13 18:49:00 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const apiKey = "082d96417c948e49c32aac5a6253138b";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener("click", () => {
+  getWeatherByCity(cityInput.value);
+});
+cityInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    getWeatherByCity(cityInput.value);
+  }
+});
+
+async function getWeatherByCity(city) {
+  if (!city) return alert("Please enter a city name");
+
+  try {
+    const url = `${baseUrl}?q=${city}&appid=${apiKey}&units=metric&lang=en`;
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("City not found");
+
+    const data = await response.json();
+
+    document.getElementById(
+      "city"
+    ).textContent = `${data.name}, ${data.sys.country}`;
+    document.getElementById("temperature").textContent = `${data.main.temp}°C`;
+    document.getElementById("description").textContent =
+      data.weather[0].description;
+    document.getElementById(
+      "humidity"
+    ).textContent = `Humidity: ${data.main.humidity}%`;
+    document.getElementById(
+      "wind"
+    ).textContent = `Wind: ${data.wind.speed} m/s`;
+
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    const weatherIcon = document.getElementById("weather-icon");
+    weatherIcon.src = iconUrl;
+    weatherIcon.style.display = "block";
+
+    const condition = data.weather[0].main.toLowerCase();
+    document.body.className = "";
+    document.body.classList.add(condition);
+  } catch (error) {
+    alert(error.message);
+  }
+}
